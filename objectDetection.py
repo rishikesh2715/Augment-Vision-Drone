b7d29f64d34dc9bc662bf3b2203bc996af2422ae
rishi <rishikesh2715@gmail.com>
2023-06-19 17:13:28 -0500

    Added distance estimation of detected objects

--- objectDetection.py
+++ objectDetection.py
@@ -1,35 +1,50 @@
 import cv2
-import argparse
-
-def parse_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description='Object Detection')
-    parser.add_argument(
-        "--webcam-resolution", 
-        default=[1280, 720], 
-        nargs = 2,
-        type=int
-    )
-    args = parser.parse_args()
-    return args 
-
+from ultralytics import YOLO
+import supervision as sv
 
 def main():
+        box_annotator = sv.BoxAnnotator(
+            thickness=2,
+            text_thickness=1,
+            text_scale=0.5,
+        )
 
-    args = parse_arguments()
-    frame_width, frame_height = args.webcam_resolution
-    
-    cap = cv2.VideoCapture(1) # 1 for webcam, 0 for camlink
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+        model = YOLO('yolov8n.pt')
 
+        for result in model.track(source="1", show=False, stream=True, classes=0):
+            frame = result.orig_img
+            detections = sv.Detections.from_yolov8(result)        
 
-    while True:
-        ret, frame = cap.read()
-        cv2.imshow('Detect', frame)
+            if result.boxes.id is not None:
+                detections.tracker_id = result.boxes.id.cpu().numpy() .astype(int)
 
-        
-        if (cv2.waitKey(30)  == 27):
-            break
+            lables = [
+                f"#{tracker_id}{class_id} {confidence:.2f}"
+                for xyxy, confidence, class_id, tracker_id
+                in detections
+            ]
+            frame = box_annotator.annotate(scene=frame, detections=detections, labels=lables)
+
+            if detections.xyxy.any():
+                [x1, y1, x2, y2] = detections.xyxy[0]
+                               
+                #Camera parameters
+                focal_length = 730  # C920 webcam focal 
+                drone_real_width = 0.41  # Tello width
+
+                drone_pixel_width = x2 - x1
+                distance = (drone_real_width * focal_length) / drone_pixel_width
+                
+                print(f"{distance:.2f} m")
+
+            cv2.imshow("frame", frame)
+            if (cv2.waitKey(1) & 0xFF) == ord("q"):
+                    break
+
 
 if __name__ == "__main__":
     main()
+            
+        
+    
+                    
