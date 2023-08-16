3f22ea4ffe955e10368893b0af1a514c54ed22ae
rishi <rishikesh2715@gmail.com>
2023-08-15 21:09:13 -0500

    added code to upscale frame to 1440p

--- main/display_heading.py
+++ main/display_heading.py
@@ -25,17 +25,20 @@ camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M','J','P','G'))
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
-width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = camera.get(cv2.CAP_PROP_FPS)
-
-print (width, height, fps) # fps is printing 0s?
+target_width = 2560    
+target_height = 1440
 
 while (1):
+    # read the camera frames
     retval, im = camera.read()
-    cv2.imshow("image", im)
+    if not retval: break
 
-    k = cv2.waitKey(1) & 0xff # Press 'ESC' for exiting video
+    #upscale the frame from 1080p to 1440p
+    resized_frame = cv2.resize(im, (target_width, target_height)) 
+    cv2.imshow("image", resized_frame)
+
+    # Press 'ESC' for exiting video
+    k = cv2.waitKey(1) & 0xff 
     if k == 27:
         break
 
