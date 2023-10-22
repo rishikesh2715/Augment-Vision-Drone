4456edb7fea9ddee6e884546b713b09891771591
rishi <rishikesh2715@gmail.com>
2023-10-21 23:52:12 -0500

    added optimization for the openCV cam test

--- main/camTest.py
+++ main/camTest.py
@@ -1,11 +1,16 @@
 import cv2
 
 # Open the webcam
-cap = cv2.VideoCapture(2)
+camera = cv2.VideoCapture(2, cv2.CAP_DSHOW)
+camera.set(cv2.CAP_PROP_FPS, 30.0)
+camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('m','j','p','g'))
+camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M','J','P','G'))
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 while True:
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    ret, frame = camera.read()
 
     # Display the resulting frame
     cv2.imshow('Webcam Feed', frame)
